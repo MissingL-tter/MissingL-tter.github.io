fb58bd6ebd469dfea0263f2ccf936949b0755553
MissingL-tter <434a44@gmail.com>
2017-08-08 23:01:22 -0400

    Fixed collapsing header

--- scripts/responsive.js
+++ scripts/responsive.js
@@ -1,16 +1,17 @@
+$content = $('.content');
 var $title = $('.title');
 var $header = $('.header');
 
 $(window).scroll( function() {
 
     var top = $(this).scrollTop();
-    if (top >= $header.position().top & $title[0].style.display != "none") {
-        $title[0].style.display = "none";
-        $header[0].classList.add("w3-top");
+    if (top >= $header.position().top & $title.css('display') != 'none') {
+        $title.css('display', 'none');
+        $header[0].classList.add('w3-top');
         $content.css('margin-top', $title.height() + $header.height());
     } else if (top <= $title.height()) {
-        $title[0].style.display = "block";
-        $header[0].classList.remove("w3-top");
+        $title.css('display', 'block');
+        $header[0].classList.remove('w3-top');
         $content.css('margin-top', 0);
     }
 });
