e781a3c55f81f8fe55ef97a2e451468471e0d729
MissingL-tter <434a44@gmail.com>
2019-11-27 13:58:28 -0500

    push hash state

--- scripts/index.js
+++ scripts/index.js
@@ -4,36 +4,46 @@ import hljs from 'highlight.js';
 // Tabs Init
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.tabs');
-    var instances = M.Tabs.init(elems, {});
+
+    function updateURL() {
+        if (this.index == 0) {
+            window.history.pushState(null, "", "#about")
+        }
+        else {
+            window.history.pushState(null, "", "#projects")
+        }
+    }
+
+    M.Tabs.init(elems, {onShow:updateURL});
 });
 
 // Tooltip Init
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.tooltipped');
-    var instances = M.Tooltip.init(elems, {});
+    M.Tooltip.init(elems, {});
 });
 
 // Scrollspy
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.scrollspy');
-    var instances = M.ScrollSpy.init(elems, {activeClass: 'show-active', scrollOffset: 176});
+    M.ScrollSpy.init(elems, {activeClass: 'show-active', scrollOffset: 176});
 });
 
 //Materialbox
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, {});
+    M.Carousel.init(elems, {});
 });
 
 //Materialbox
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.materialboxed');
-    var instances = M.Materialbox.init(elems, {});
+    M.Materialbox.init(elems, {});
 });
 
 hljs.initHighlightingOnLoad();
 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, {});
+    M.Modal.init(elems, {});
 });
